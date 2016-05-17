075f85c3ee60f60f93535b4a367eda4c07c01998
Gregg Bookspan <gbookspan@seeitourway.org>
2016-05-17 14:04:54 -0700

    Style about about & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,19 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About component</h3>
-//     )
-//   }
-// });
-
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built on React.</p>
+      <p>Some elements we use on this site:</p>
+      <ol>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This is the JavaScript framework
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This is used to search weather data by city name
+        </li>
+      </ol>
     </div>
   )
 };
